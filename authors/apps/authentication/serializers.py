f969fff4301226a436c2bb1c63b14d5024be4914
Caleb Rotich <calebrotich10@gmail.com>
2019-01-31 12:32:37 +0300

    feature(social auth): add google and facebook authentication
    - allow user login by google
    - allow user login by facebook
    - add tests for social auth
    - code refactor
    
    [Finishes #163388251]

--- authors/apps/authentication/serializers.py
+++ authors/apps/authentication/serializers.py
@@ -156,3 +156,12 @@ class UserSerializer(serializers.ModelSerializer):
         instance.save()
 
         return instance
+
+
+class SocialAuthSerializer(serializers.Serializer):
+    """ Accepts provider, access token , and access_token_secret"""
+    provider = serializers.CharField(max_length=255, required=True)
+    access_token = serializers.CharField(
+        max_length=4096, required=True, trim_whitespace=True)
+    access_token_secret = serializers.CharField(
+        max_length=4096, required=False, trim_whitespace=True)
